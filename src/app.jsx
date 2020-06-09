3180bbe83388571b1c449ed99281f5680dc39af0
SongShuhang <java0120@qq.com>
2020-06-09 21:01:42 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+/*
+* @Author: SongShuhang
+* @Date:   2020-06-09 18:59:02
+* @Last Modified by:   SongShuhang
+* @Last Modified time: 2020-06-09 19:19:06
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+	<div>
+		
+		<h1>hello, World!</h1>
+	</div>,
+	document.getElementById('app')
+);
